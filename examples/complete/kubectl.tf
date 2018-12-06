c4f37aa1afe081642be0d76f8d786b137b232c53
aknysh <andriy.knysh@gmail.com>
2018-12-06 17:34:34 -0500

    Update README

--- examples/complete/kubectl.tf
+++ examples/complete/kubectl.tf
@@ -1,11 +1,12 @@
 ###########################################################################################################################################
 #
-# NOTE: To automatically apply the Kubernetes configuration to the cluster, the requirements outlined here must be met:
+# NOTE: To automatically apply the Kubernetes configuration to the cluster (which allows the worker nodes to join the cluster),
+# the requirements outlined here must be met:
 # https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#preparation
 # https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#configuring-kubectl-for-eks
 # https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#required-kubernetes-configuration-to-join-worker-nodes
 #
-# If you don't want to automatically apply the Kubernetes configuration, set `var.apply_config_map_aws_auth` to "false"
+# If you want to automatically apply the Kubernetes configuration, set `var.apply_config_map_aws_auth` to "true"
 #
 ###########################################################################################################################################
 
--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -147,6 +147,6 @@ variable "availability_zones" {
 
 variable "apply_config_map_aws_auth" {
   type        = "string"
-  default     = "true"
+  default     = "false"
   description = "Whether to generate local files from `kubeconfig` and `config_map_aws_auth` and perform `kubectl apply` to apply the ConfigMap to allow the worker nodes to join the EKS cluster"
 }
