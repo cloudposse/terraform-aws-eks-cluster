c6e7adea8e522f40f0c634e8b2e8cb97408a5f00
Vitaly Markov <v.y.markov@gmail.com>
2019-11-10 21:39:27 +0000

    Fix typo

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -20,7 +20,7 @@ locals {
 
   # Unfortunately, most_recent (https://github.com/cloudposse/terraform-aws-eks-workers/blob/34a43c25624a6efb3ba5d2770a601d7cb3c0d391/main.tf#L141)
   # variable does not work as expected, if you are not going to use custom ami you should
-  # enforce usage of eks_worker_ami_name_filter variable to set the rigth kubernetes version for EKS workers,
+  # enforce usage of eks_worker_ami_name_filter variable to set the right kubernetes version for EKS workers,
   # otherwise will be used the first version of Kubernetes supported by AWS (v1.11) for EKS workers but
   # EKS control plane will use the version specified by kubernetes_version variable.
   eks_worker_ami_name_filter = "amazon-eks-node-${var.kubernetes_version}*"
