1492a368dca842d3befef43cb0c232e91b02bc36
Shaun Cutts <shaunc@factfiber.com>
2019-12-16 23:40:50 -0500

    wait for cluster to become responsive

--- auth.tf
+++ auth.tf
@@ -137,7 +137,7 @@ resource "null_resource" "apply_configmap_auth" {
 
       echo 'Applying Auth ConfigMap with kubectl...'
       aws eks update-kubeconfig --name=${local.cluster_name} --region=${var.region} --kubeconfig=${var.kubeconfig_path} ${var.aws_eks_update_kubeconfig_additional_arguments}
-      kubectl version --kubeconfig ${var.kubeconfig_path}
+      until kubectl version --kubeconfig ${var.kubeconfig_path} >/dev/null; do sleep 4; done
       kubectl apply -f ${local.configmap_auth_file} --kubeconfig ${var.kubeconfig_path}
       echo 'Applied Auth ConfigMap with kubectl'
     EOT
