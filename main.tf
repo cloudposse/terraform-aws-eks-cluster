6fc6131b2afb770b91e0fb3dfc024e0558f1bb5f
Martin Dojcak <martin.dojcak@lablabs.io>
2020-08-13 09:54:04 +0200

    Update label mpdule to support 0.13

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
   namespace   = var.namespace
   stage       = var.stage
   environment = var.environment
