2df80fe0d1d6300ea2dfd794202f999d158e1382
Dan Meyers <danjbh@users.noreply.github.com>
2020-08-26 17:18:53 -0700

    Bumping AWS provider version to ~> 3.0.0

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.0"
 
   required_providers {
-    aws        = "~> 2.0"
+    aws        = "~> 3.0"
     template   = "~> 2.0"
     null       = "~> 2.0"
     local      = "~> 1.3"
