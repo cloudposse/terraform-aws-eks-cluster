63f974523434d0e13388e52afd32324243e11f94
Nuru <Nuru@users.noreply.github.com>
2020-08-28 00:34:18 -0700

    Restore incorrectly deleted parameter

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -61,6 +61,7 @@ module "subnets" {
 module "eks_cluster" {
   source = "../../"
 
+  region                       = var.region
   vpc_id                       = module.vpc.vpc_id
   subnet_ids                   = concat(module.subnets.private_subnet_ids, module.subnets.public_subnet_ids)
   kubernetes_version           = var.kubernetes_version
