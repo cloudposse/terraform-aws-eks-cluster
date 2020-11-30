a970e92a9f485f49839291306905d2e8687fd801
devslx <devang.lakhani@sellics.com>
2020-11-30 12:52:28 +0100

    fix for aws_iam_role.default[0]

--- iam.tf
+++ iam.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 locals {
-  eks_service_role = var.eks_cluster_service_role_arn != null ? var.eks_cluster_service_role_arn : aws_iam_role.default.arn
+  eks_service_role = var.eks_cluster_service_role_arn != null ? var.eks_cluster_service_role_arn : aws_iam_role.default[0].arn
 }
 resource "aws_iam_role" "default" {
   count              = var.eks_cluster_service_role_arn != null && local.enabled ? 1 : 0
