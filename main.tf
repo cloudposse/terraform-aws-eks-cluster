ffa73d38442eed607dfcf8d835e75794f11438db
Tanuj Dwivedi <68149299+tanujd11@users.noreply.github.com>
2021-01-22 14:57:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_eks_cluster" "default" {
 
   vpc_config {
     security_group_ids      = [join("", aws_security_group.default.*.id)]
-    subnet_ids              = var.subnet_ids
+    subnet_ids              = sort(var.subnet_ids)
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
     public_access_cidrs     = var.public_access_cidrs
