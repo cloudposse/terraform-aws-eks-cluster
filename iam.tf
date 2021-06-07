b3ac4c85b7582d5124ef165d1a33361999af71bd
nitrocode <nitrocode@users.noreply.github.com>
2021-06-07 18:59:51 -0400

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -15,7 +15,7 @@ data "aws_iam_policy_document" "assume_role" {
 locals {
   eks_service_role = var.eks_cluster_service_role_arn != null ? var.eks_cluster_service_role_arn : join("", aws_iam_role.default.*.arn)
 
-  create_eks_service_role = var.eks_cluster_service_role_arn != null && local.enabled
+  create_eks_service_role = var.eks_cluster_service_role_arn == null && local.enabled
 }
 
 resource "aws_iam_role" "default" {
