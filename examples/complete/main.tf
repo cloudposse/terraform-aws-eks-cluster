9434ef89c588b7b7ef691b3ea5e96e999fc269bb
Savan Nahar <savannahar68@gmail.com>
2021-07-15 23:41:55 +0530

    Update: Move from deprecated list used in "cloudposse/dynamic-subnets/aws" 0.38.0 to latest version
    While running complete example terraform validate (version 0.15+) will throw error because  cloudposse/dynamic-subnets/aws" 0.38.0 version uses list datatype which is deprecated
    
    ```
    output "nat_ips" {
      description = "IP Addresses in use for NAT"
      value       = coalescelist(aws_eip.default.*.public_ip, aws_eip.nat_instance.*.public_ip, data.aws_eip.nat_ips.*.public_ip, list(""))
    }
    ```
    
    https://github.com/cloudposse/terraform-aws-dynamic-subnets/blob/0.38.0/outputs.tf
    
    Using the latest version 0.39+ will resolve this error

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -44,7 +44,7 @@ module "vpc" {
 
 module "subnets" {
   source  = "cloudposse/dynamic-subnets/aws"
-  version = "0.38.0"
+  version = "0.39.3"
 
   availability_zones              = var.availability_zones
   vpc_id                          = module.vpc.vpc_id
