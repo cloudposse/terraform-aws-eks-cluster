8611da951270e74bb26205beb9003989703b02e5
aknysh <andriy.knysh@gmail.com>
2022-01-06 20:43:09 -0500

    Udate Security Group

--- security-group.tf
+++ security-group.tf
@@ -56,8 +56,8 @@ module "aws_security_group" {
   rules       = var.additional_security_group_rules
   rule_matrix = local.rule_matrix
 
-  # If `var.create_security_group=true`, `module "aws_security_group"` will create a new Security Group and apply all the rules to it
-  # If `var.create_security_group=false`, `module "aws_security_group"` will use the EKS cluster managed Security Group and apply all the rules to it
+  # If `var.create_security_group=true`, `module "aws_security_group"` will create a new Security Group and apply all the rules to it - use this with unmanaged worker nodes
+  # If `var.create_security_group=false`, `module "aws_security_group"` will use the EKS cluster managed Security Group and apply all the rules to it - use this with managed worker nodes
   target_security_group_id = var.create_security_group ? [] : [join("", aws_eks_cluster.default.*.vpc_config.0.cluster_security_group_id)]
 
   vpc_id = var.vpc_id
