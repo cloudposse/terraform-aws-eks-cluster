f422d4d6e57d4a0f62ff97ee3bd3eb8c19cc2697
RB <7775707+nitrocode@users.noreply.github.com>
2024-11-27 19:01:40 -0600

    Apply suggestions from code review
    Co-authored-by: Ihor Urazov <z0rc@users.noreply.github.com>

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.75"
+      version = ">= 5.77"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
