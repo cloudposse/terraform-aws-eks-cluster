8468ce2901f7d92ef9bc65eb9fe5d2441650b232
RoseSecurity <michaeljrosenfeld22@gmail.com>
2025-02-05 11:41:43 -0500

    fix: update role arn logic

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_eks_cluster" "default" {
     content {
       enabled       = local.auto_mode_enabled
       node_pools    = local.auto_mode_enabled ? try(compute_config.value.node_pools, []) : null
-      node_role_arn = local.auto_mode_enabled && length(try(compute_config.value.node_pools, [])) > 0 ? try(compute_config.value.node_role_arn, aws_iam_role.eks_auto[0].arn, null) : null
+      node_role_arn = local.auto_mode_enabled && length(try(compute_config.value.node_pools, [])) > 0 ? try(compute_config.value.node_role_arn, aws_iam_role.default[0].arn, null) : null
     }
   }
 
