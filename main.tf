90498be72d01049211f3f9fd91b62c3d471ffbfa
RoseSecurity <michaeljrosenfeld22@gmail.com>
2025-02-19 16:53:56 -0500

    fix: logic update

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "aws_eks_cluster" "default" {
 
   # EKS Auto Mode
   dynamic "compute_config" {
-    for_each = local.auto_mode_enabled && (var.cluster_compute_config) > 0 ? [var.cluster_compute_config] : []
+    for_each = local.auto_mode_enabled && length(var.cluster_compute_config) > 0 ? [var.cluster_compute_config] : []
 
     content {
       enabled       = local.auto_mode_enabled
