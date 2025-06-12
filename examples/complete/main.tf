c7240ff2f16773b33d57a3b4b9e798279db94e4d
Romulo Franca <romulo2franca@gmail.com>
2025-06-12 12:47:36 -0300

    fix: streamline EKS node group output values and utilize local variable for auto mode

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -20,6 +20,8 @@ data "aws_iam_session_context" "current" {
 locals {
   enabled = module.this.enabled
 
+  auto_mode_enabled = var.cluster_auto_mode_enabled
+
   private_ipv6_enabled = var.private_ipv6_enabled
 
   # The usage of the specific kubernetes.io/cluster/* resource tags below are required
@@ -114,12 +116,12 @@ module "eks_cluster" {
   addons_depends_on                     = [module.eks_node_group]
   bootstrap_self_managed_addons_enabled = var.bootstrap_self_managed_addons_enabled
   upgrade_policy                        = var.upgrade_policy
-  zonal_shift_config                     = var.zonal_shift_config
+  zonal_shift_config                    = var.zonal_shift_config
 
-  cluster_auto_mode_enabled               = var.cluster_auto_mode_enabled
+  cluster_auto_mode_enabled = local.auto_mode_enabled
 
-  create_node_role                        = var.create_node_role
-  node_pools                              = var.node_pools
+  create_node_role = var.create_node_role
+  node_pools       = var.node_pools
 
   access_entry_map = local.access_entry_map
   access_config = {
@@ -140,7 +142,7 @@ module "eks_cluster" {
 }
 
 module "eks_node_group" {
-  count   = var.cluster_auto_mode_enabled ? 0 : 1
+  enabled = local.auto_mode_enabled
   source  = "cloudposse/eks-node-group/aws"
   version = "3.2.0"
 
